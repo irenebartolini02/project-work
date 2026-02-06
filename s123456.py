be5ffef92aaa1852834777aff9e6a449ca48892b
irenebartolini02 <s345905@studenti.polito.it>
2026-02-06 13:03:53 +0100

    first_ commit

---
+++ s123456.py
@@ -0,0 +1,8 @@
+from Problem import Problem
+
+def solution(p:Problem):
+    p=Problem()
+    # TODO: implement your solution here
+    # In the enf of the code for your solution, make sure that the path has the format [(c1, g1), (c2, g2), …, (cN, gN), (0, 0)]
+    path = [(1, 75), (0, 0)]
+    return path
---
+++ src/README.md
@@ -0,0 +1,2 @@
+# SRC
+This folder contains the code that you need to run the final solution
